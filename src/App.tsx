907a42658a006b5565c55671586ab4f317c0a1e5
Mateusz Naumowicz <naumowicz@protonmail.com>
2021-04-11 19:20:21 +0200

    using list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import AntdCheckbox from './components/AntdCheckbox'
 import Header from './components/Header'
+import AntdList from './components/AntdList';
 
+const list = ['a', 'b', 'c'];
 
 const App = () => {
 	return (
 		<>
-			<Header text={recipe.getName()}/>
+			<Header text="Lista Przepisów"/>
+			<AntdList list={list}/>
 			{/* {recipe.getIngredients().forEach(ingredient => {
 				<AntdCheckbox text={ingredient} />
 			})}			 */}
