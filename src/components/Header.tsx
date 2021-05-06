24aec85ac4039b4c778919cddd9f8fd9b4a61030
Mateusz Naumowicz <naumowicz@protonmail.com>
2021-05-06 22:08:58 +0200

    Updates

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header = ({text}: {text: string}) => {
-	return (
-		<header>
-			<h1>{text}</h1>
-		</header>
-	)
-}
-
-export default Header;
--- src/interfaces/recipeInterface.ts
+++
@@ -1,3 +0,0 @@
-export default interface recipeInterface {
-	name: string; source: string; ingredients: Array<string>; tools: Array<string>; actions: Array<string>
-}
